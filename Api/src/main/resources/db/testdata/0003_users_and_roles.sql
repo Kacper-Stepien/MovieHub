79e5189141099903808fcd113acffc139f9ce9de
RobertNeat <robertgoogl@gmail.com>
2025-03-10 15:27:56 +0100

    fix: change users passwords in sql file to known

--- Api/src/main/resources/db/testdata/0003_users_and_roles.sql
+++ Api/src/main/resources/db/testdata/0003_users_and_roles.sql
@@ -2,12 +2,16 @@
 insert into
     users (email, password, first_name, last_name)
 values
-    ('admin@example.com', '$2a$12$M5uX2zGbXpO7HREUkAdL/.FqO9vXjaySHBnVOULmk/dmspb7vAfcW', 'Admin', 'Example'),
-    ('user@example.com', '$2y$10$H9B0umVBgUUE4AZVvzvomONcA.aXjo6ZaL7Wuvku719Nl8PGESsN6', 'John', 'Doe'),
-    ('anna.kowalska@example.com', '$2y$10$H9B0umVBgUUE4AZVvzvomONcA.aXjo6ZaL7Wuvku719Nl8PGESsN6', 'Anna', 'Kowalska'),
-    ('jan.nowak@example.com', '$2y$10$H9B0umVBgUUE4AZVvzvomONcA.aXjo6ZaL7Wuvku719Nl8PGESsN6', 'Jan', 'Nowak'),
-    ('ola@gmail.com', '$2y$10$H9B0umVBgUUE4AZVvzvomONcA.aXjo6ZaL7Wuvku719Nl8PGESsN6', 'Ola', 'Kowalska');
-
+    ('admin@example.com', '$2b$12$IPfWYIgo0ouALrPr/t02xOx67YHFkfoQMmLbRcCKoRPsfPuobxOB.', 'Admin', 'Example'),-- <<pass: admin@example
+    ('user@example.com', '$2b$12$f5a916UYzz6OfqCgVncvqOjlSRaLfBIt6dzBZ1EvDt/aFMdxf32UK', 'John', 'Doe'),-- <<pass: user@example
+    ('anna.kowalska@example.com', '$2b$12$N6gwumWzngcRuyLGAajZquvJ.DuzRMcbIAOnjX2Mwxvej.LGEwaEK', 'Anna', 'Kowalska'),-- <<pass: anna.kowalska@example
+    ('jan.nowak@example.com', '$2b$12$rxIzfhwuSseab6w.QJtXHugnKitGWjY31XHK8wccuPGb8XfCRWYEC', 'Jan', 'Nowak'),-- <<pass: jan.nowak@example
+    ('ola@gmail.com', '$2b$12$xDp4L4c7euZI5iSObhFDw.9Qij9ftfRhnzWd07gWsevctbWoDX5rm', 'Ola', 'Kowalska');-- <<pass: ola@gmail
+-- admin@example.com -- $2a$12$M5uX2zGbXpO7HREUkAdL/.FqO9vXjaySHBnVOULmk/dmspb7vAfcW << original password
+-- user@example.com -- $2y$10$H9B0umVBgUUE4AZVvzvomONcA.aXjo6ZaL7Wuvku719Nl8PGESsN6 << original password
+-- anna.kowalska@example.com -- $2y$10$H9B0umVBgUUE4AZVvzvomONcA.aXjo6ZaL7Wuvku719Nl8PGESsN6 << original password
+-- jan.nowak@example.com -- $2y$10$H9B0umVBgUUE4AZVvzvomONcA.aXjo6ZaL7Wuvku719Nl8PGESsN6 << original password
+-- ola@gmail.com -- $2y$10$H9B0umVBgUUE4AZVvzvomONcA.aXjo6ZaL7Wuvku719Nl8PGESsN6 << original password
 
 -- Dodanie ról użytkowników
 insert into
